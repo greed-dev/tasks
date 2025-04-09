f53d52c3c5608d8579d553dc20a48f57bbdec353
Greed <greed@greed.dev>
2025-04-09 13:45:20 -0400

    solved components

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
 
-function Doubler(): React.JSX.Element {
+interface ControlProps {
+    dhValue: number;
+    setDhValue: React.Dispatch<React.SetStateAction<number>>;
+}
+
+function Doubler({ dhValue, setDhValue }: ControlProps): React.JSX.Element {
     return (
         <Button
             onClick={() => {
@@ -14,7 +18,7 @@ function Doubler(): React.JSX.Element {
     );
 }
 
-function Halver(): React.JSX.Element {
+function Halver({ dhValue, setDhValue }: ControlProps): React.JSX.Element {
     return (
         <Button
             onClick={() => {
@@ -27,14 +31,16 @@ function Halver(): React.JSX.Element {
 }
 
 export function DoubleHalf(): React.JSX.Element {
+    const [dhValue, setDhValue] = useState<number>(10);
+
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Doubler dhValue={dhValue} setDhValue={setDhValue}></Doubler>
+            <Halver dhValue={dhValue} setDhValue={setDhValue}></Halver>
         </div>
     );
 }
